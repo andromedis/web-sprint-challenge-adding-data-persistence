1e164a287a1b4ee7277395f0d970cdeeb034c994
Rebecca Peckman <rebeccapeckman@gmail.com>
2021-04-16 13:07:50 +0100

    setup, first migration

---
+++ data/migrations/20210416113046_01-first-migration.js
@@ -0,0 +1,49 @@
+exports.up = function(knex) {
+    return knex.schema
+    .createTable('projects', table => {
+        table.increments('project_id')
+        table.string('project_name', 128).notNullable()
+        table.string('project_description', 255)
+        table.boolean('project_completed').notNullable().defaultTo(0)
+    })
+    .createTable('resources', table => {
+        table.increments('resource_id')
+        table.string('resource_name', 128).notNullable().unique()
+        table.string('resource_description', 255)
+    })
+    .createTable('tasks', table => {
+        table.increments('task_id')
+        table.string('task_description', 128).notNullable()
+        table.string('task_notes', 255)
+        table.boolean('task_completed').notNullable().defaultTo(0)
+        table.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('project_id')
+            .inTable('projects')
+            .onDelete('RESTRICT')
+    })
+    .createTable('project_resources', table => {
+        table.increments('project_resource_id')
+        table.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('project_id')
+            .inTable('projects')
+            .onDelete('RESTRICT')
+        table.integer('resource_id')
+            .unsigned()
+            .notNullable()
+            .references('resource_id')
+            .inTable('resources')
+            .onDelete('RESTRICT')
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema
+        .dropTableIfExists('project_resources')
+        .dropTableIfExists('tasks')
+        .dropTableIfExists('resources')
+        .dropTableIfExists('projects')
+};
