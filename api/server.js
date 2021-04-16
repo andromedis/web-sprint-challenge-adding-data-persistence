613d69d5925c1e5b372331b50b3f42e9abecd255
Rebecca Peckman <rebeccapeckman@gmail.com>
2021-04-16 13:32:29 +0100

    server setup

--- api/server.js
+++ api/server.js
@@ -1,1 +1,25 @@
-// build your server here and require it from index.js
+// Imports
+const express = require('express')
+const projectsRouter = require('./project/router')
+const resourcesRouter = require('./resource/router')
+const tasksRouter = require('./task/router')
+
+// Express server instance
+const server = express()
+
+// Middleware
+server.use(express.json())
+server.use('/api/projects', projectsRouter)
+server.use('/api/resources', resourcesRouter)
+server.use('/api/tasks', tasksRouter)
+
+// Error handling
+server.use((err, req, res, next) => {
+    res.status(500).json({
+        message: err.message, 
+        stack: err.stack
+    })
+})
+
+// Exports
+module.exports = server
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // start your server here
+const server = require('./api/server')
+
+const PORT = process.env.PORT || 1234
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
